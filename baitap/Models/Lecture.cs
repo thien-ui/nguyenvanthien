df8494f9331a716ed359027b0fe72e4f06a99fec
thien-ui <148088738+thien-ui@users.noreply.github.com>
2023-12-16 01:14:59 +0700

    up
    up

---
+++ baitap/Models/Lecture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap.Models
+{
+    internal class Lecture
+    {
+    }
+}
---
+++ baitap/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap.Models
+{
+    internal class Student
+    {
+
+        public int ID { get; set; }
+
+        public int MSSV { get; set; }
+
+    }
+}
